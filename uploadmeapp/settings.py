ae9dc324bf3bfe146e1a336011e459c5487726f7
teohoyos1 <60853412+teohoyos1@users.noreply.github.com>
2022-04-29 01:28:27 -0500

    Update settings.py
    Fixing

--- uploadmeapp/settings.py
+++ uploadmeapp/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5u%#p8+6t=l^-yfvtjbq9ds^y4w6c*a#zmmm@j7bwom^e96xvr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(getenv('DEBUG')) == "1"
